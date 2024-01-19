c38d7fa85b9ff2a622fa7664ed4f8a214496ac81
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-01-19 16:43:28 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var calculator = require('./calc');
-var x=50,y=10;
-console.log("Addition of 50 and 10 is ",+calculator.add(x,y));
-console.log("Subtraction of 50 and 10 is ",+calculator.sub(x,y));
-console.log("Multiplication of 50 and 10 is ",+calculator.mult(x,y));
-console.log("Division of 50 and 10 is ",+calculator.div(x,y));
